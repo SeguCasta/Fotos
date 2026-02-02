dc5f2ac3c1d06fc3ea28e29ed459ecc03ed854e8
SeguCasta <seguCasta@gmail.com>
2026-02-02 12:34:52 +0100

    cambio

---
+++ Inserts.sql
@@ -0,0 +1,38 @@
+/*
+CREATE TABLE dbo.CopiaFicheros (
+    Id              INT IDENTITY PRIMARY KEY,
+    RutaOrigen      NVARCHAR(500) NOT NULL,
+    NombreFichero   NVARCHAR(255) NOT NULL,
+    RutaDestino     NVARCHAR(500) NOT NULL,
+    Copiado         BIT NOT NULL DEFAULT 0,
+    FechaCopia      DATETIME NULL,
+    Error           NVARCHAR(1000) NULL
+);
+
+*/
+--Insert into CopiaFicheros
+select f1.ruta as RutaOrigen, f1.nombre as NombreFichero, 'C:\Users\seguc\OneDrive\Pictures\Galería de Samsung' as RutaDestino, 0, '01/01/1999',NULL
+from FotosDCIM f1
+where
+1=1
+
+and ruta not like '%Galería de Samsung%'
+
+and ruta not like '%pictures\NO_Galeria%'
+
+--and ruta not like '%pictures\Albunes%'
+
+
+and not exists (
+	select nombre 
+	from fotosdcim
+	where ruta<>f1.ruta and nombre = f1.nombre
+	)
+
+and ruta like 'C:\Users\seguc\OneDrive\Pictures\Álbum de cámara%'
+order by ruta, Nombre
+
+/*
+select * from CopiaFicheros
+*/
+
